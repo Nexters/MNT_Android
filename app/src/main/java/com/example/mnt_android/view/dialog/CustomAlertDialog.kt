3d05204a1fee6eb03b787725be0cef721513a8ed
wishJinit <wishjinit@gmail.com>
2020-02-19 17:34:15 +0900

    custom dialog 수정
    - Custom Alert Dialog : btn listener 추가, 가운데정렬

--- app/src/main/java/com/example/mnt_android/view/dialog/CustomAlertDialog.kt
+++ app/src/main/java/com/example/mnt_android/view/dialog/CustomAlertDialog.kt
@@ -9,14 +9,20 @@ import androidx.fragment.app.DialogFragment
 import com.example.mnt_android.R
 import kotlinx.android.synthetic.main.dialog_alert.view.*
 
-class CustomAlertDialog(private val dialogMsg: String, private val btnMsg: String? = "확인") : DialogFragment() {
+class CustomAlertDialog(
+    private val dialogMsg: String, private val btnMsg: String? = "확인",
+    private val confirmOnClickListener: () -> Unit = {}
+) : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val view = it.layoutInflater.inflate(R.layout.dialog_alert, null).apply {
                 question_tv.text = dialogMsg
                 confirm_btn.run {
                     text = btnMsg
-                    setOnClickListener { dismiss() }
+                    setOnClickListener {
+                        confirmOnClickListener()
+                        dismiss()
+                    }
                 }
             }
             AlertDialog.Builder(it).apply {
