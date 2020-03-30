32e404894410034b62393e68594fff88c5b42911
wishJinit <wishjinit@gmail.com>
2020-03-31 08:35:04 +0900

    시작 전 참여자리스트 이미지 매칭
    - 관리자 : 칵테일 이모티콘
    - 참여자 : 입장순대로 얼굴 이모티콘

--- app/src/main/java/com/example/mnt_android/view/viewholder/ApplicantListViewHolder.kt
+++ app/src/main/java/com/example/mnt_android/view/viewholder/ApplicantListViewHolder.kt
@@ -3,7 +3,10 @@ package com.example.mnt_android.view.viewholder
 import android.view.View
 import androidx.fragment.app.FragmentActivity
 import com.example.mnt_android.base.BaseViewHolder
+import com.example.mnt_android.binding.setFaceProfileSrc
+import com.example.mnt_android.binding.setFruitProfileSrc
 import com.example.mnt_android.extension.isFalse
+import com.example.mnt_android.extension.isTrue
 import com.example.mnt_android.service.model.Applicant
 import com.example.mnt_android.view.dialog.ConfirmDialog
 import kotlinx.android.synthetic.main.item_applicant.view.*
@@ -21,6 +24,11 @@ class ApplicantListViewHolder(
     override fun onBind(position: Int, data: Any) {
         val applicant = data as Applicant
         itemView.run {
+            if (applicant.isCreater.isTrue) {
+                setFruitProfileSrc(profile_iv, 0)
+            } else {
+                setFaceProfileSrc(profile_iv, position)
+            }
             applicant_name_tv.text = applicant.user.name
             exit_btn.setOnClickListener {
                 val supportFragmentManager = (context as FragmentActivity).supportFragmentManager
